18fe33e3b561c48e18f6a4b6118f56a9febf3dac
David Buckley <dbuckl02@mail.uoguelph.ca>
2018-01-11 20:24:13 -0500

    fix linting

--- src/boids/cli.py
+++ src/boids/cli.py
@@ -5,6 +5,5 @@ import click
 @click.option('--height', default=480, type=int)
 @click.option('--n', default=50, type=int)
 @click.option('--cycles', default=-1, type=int)
-@click.pass_context
-def cli(ctx: click.core.Context, width: int, height: int, n: int, cycles: int) -> None:
-	pass
+def cli(width: int, height: int, num_boids: int, cycles: int) -> None:
+    print(width, height, num_boids, cycles)
